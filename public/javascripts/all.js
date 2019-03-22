c2f090326d0e6239a047dfb821ef900eb1fa008c
andyjumars <andyjumars@hotmail.com>
2019-03-22 17:13:40 +0800

    gh

---
+++ public/javascripts/all.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var helpers = {
+  getQuery: function getQuery() {
+    console.log('getQuery');
+    return 'getQuery';
+  }
+};
+
+console.log('return ' + helpers.getQuery());
+console.log('return ' + helpers.getQuery() + ' line 2');
+// ES6 範例程式碼
+//# sourceMappingURL=all.js.map
